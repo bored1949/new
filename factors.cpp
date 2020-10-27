133e0b03b3f4fda7f5367ab06ee76639fc90681b
bored1949 <61618921+bored1949@users.noreply.github.com>
2020-10-27 16:28:57 +0530

    Update factors.cpp

--- factors.cpp
+++ factors.cpp
@@ -11,9 +11,19 @@ void sieve(int n)
             pf[j].push_back(i);
         }
     }
+    for(int i=1; i<=n; i++)
+    {
+        cout << i << ": ";
+        for(int j=0; j<pf[i].size(); j++)
+        {
+            cout << pf[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 int main()
 {
+    cout << "enter a number to find factors until" << endl;
     int n;
     cin >> n;
     sieve(n);
